ca9c8bff18c686d5e1ace023bbf97650d0f5d4de
Choudhary <juhi.choudhary@capgemini.com>
2018-10-17 16:36:21 +0530

    forms example

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactFormComponent,
+    SignupFormComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
